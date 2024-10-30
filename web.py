ac5b9521768d22843a83072c95408e3ebefcbcd5
Vitor Outi <vitorcrupe@gmail.com>
2024-10-30 15:29:56 -0300

    Adição de argumento para salvar download

--- web.py
+++ web.py
@@ -3,7 +3,8 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 
-def create_chrome_driver():
+def create_driver(download_directory=None):
+    print('Inicializando Webdriver do Chrome')
     chrome_options = Options()
     
     # Configurações adicionais para estabilidade
@@ -12,6 +13,11 @@ def create_chrome_driver():
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--start-maximized")
     chrome_options.add_argument("--ignore-certificate-errors")
+    
+    # Definir pasta para downloads, se especificada
+    if download_directory:
+        prefs = {"download.default_directory": download_directory}
+        chrome_options.add_experimental_option("prefs", prefs)
     
     # Instancia o ChromeDriver com o WebDriver Manager
     driver = webdriver.Chrome(
